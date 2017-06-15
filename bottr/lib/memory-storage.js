2120b5578901215560e37d73b05880491dcad871
Sebastian Grebe <sebigrebe@googlemail.com>
2017-06-15 17:01:06 +0200

    Improvements

--- bottr/lib/memory-storage.js
+++ bottr/lib/memory-storage.js
@@ -11,38 +11,38 @@ function MemoryStorage() {
     }
   };
 
-  return function init (bot) {
+  return function init(bot) {
 
     bot.on('message_received', (message, session, next) => {
 
-      session.getUserContext = function(defaults) {
-        var context = this.memory.users[session.user] || {};
+      session.getUserContext = function (defaults) {
+        let context = this.memory.users[session.user] || {};
         return Object.assign({}, defaults || {}, context);
       }.bind(this);
 
-      session.getConversationContext = function(defaults) {
-        var context = this.memory.conversations[session.conversation] || {};
+      session.getConversationContext = function (defaults) {
+        let context = this.memory.conversations[session.conversation] || {};
         return Object.assign({}, defaults || {}, context);
       }.bind(this);
 
-      session.getAccountContext = function(defaults) {
-        var context = this.memory.accounts[session.account] || {};
+      session.getAccountContext = function (defaults) {
+        let context = this.memory.accounts[session.account] || {};
         return Object.assign({}, defaults || {}, context);
       }.bind(this);
 
-      session.updateUserContext = function(newValues) {
-        var context = this.memory.users[session.user] || {}
-        this.memory.users[session.user] = Object.assign(context, newValues)
+      session.updateUserContext = function (newValues) {
+        let context = this.memory.users[session.user] || {};
+        this.memory.users[session.user] = Object.assign(context, newValues);
       }.bind(this);
 
-      session.updateConversationContext = function(newValues) {
-        var context = this.memory.conversations[session.conversation]  || {}
-        this.memory.conversations[session.conversation] = Object.assign(context, newValues)
+      session.updateConversationContext = function (newValues) {
+        let context = this.memory.conversations[session.conversation] || {};
+        this.memory.conversations[session.conversation] = Object.assign(context, newValues);
       }.bind(this);
 
-      session.updateAccountContext = function(newValues) {
-        var context = this.memory.accounts[session.account] || {}
-        this.memory.accounts[session.account] = Object.assign(context, newValues)
+      session.updateAccountContext = function (newValues) {
+        let context = this.memory.accounts[session.account] || {};
+        this.memory.accounts[session.account] = Object.assign(context, newValues);
       }.bind(this);
 
       next();
