f0833ff474fe2de283cbe88c3985aebfcc03c32e
Sebastian Grebe <sebigrebe@googlemail.com>
2017-06-15 16:18:29 +0200

    README and improvements

--- bottr/lib/memory-storage.js
+++ bottr/lib/memory-storage.js
@@ -16,42 +16,42 @@ function MemoryStorage() {
     bot.on('message_received', (message, session, next) => {
 
       session.getUserContext = function(defaults) {
-        var context = this.memory.users[session.user] || {}
-        return Object.assign({}, defaults || {}, context)
-      }.bind(this)
+        var context = this.memory.users[session.user] || {};
+        return Object.assign({}, defaults || {}, context);
+      }.bind(this);
 
       session.getConversationContext = function(defaults) {
-        var context = this.memory.conversations[session.conversation] || {}
-        return Object.assign({}, defaults || {}, context)
-      }.bind(this)
+        var context = this.memory.conversations[session.conversation] || {};
+        return Object.assign({}, defaults || {}, context);
+      }.bind(this);
 
       session.getAccountContext = function(defaults) {
-        var context = this.memory.accounts[session.account] || {}
-        return Object.assign({}, defaults || {}, context)
-      }.bind(this)
+        var context = this.memory.accounts[session.account] || {};
+        return Object.assign({}, defaults || {}, context);
+      }.bind(this);
 
       session.updateUserContext = function(newValues) {
         var context = this.memory.users[session.user] || {}
         this.memory.users[session.user] = Object.assign(context, newValues)
-      }.bind(this)
+      }.bind(this);
 
       session.updateConversationContext = function(newValues) {
         var context = this.memory.conversations[session.conversation]  || {}
         this.memory.conversations[session.conversation] = Object.assign(context, newValues)
-      }.bind(this)
+      }.bind(this);
 
       session.updateAccountContext = function(newValues) {
         var context = this.memory.accounts[session.account] || {}
         this.memory.accounts[session.account] = Object.assign(context, newValues)
-      }.bind(this)
+      }.bind(this);
 
-      next()
+      next();
 
     })
 
-    return this
+    return this;
 
-  }.bind(this)
+  }.bind(this);
 }
 
 module.exports = MemoryStorage
