a670d3f77d7f08c75e212bac3aafb977d2ccf581
Sebastian Grebe <grebe@ifu-aachen.de>
2017-06-16 23:51:49 +0200

    Tests test

--- Dockerfile
+++ Dockerfile
@@ -11,9 +11,9 @@ RUN apt-get update && apt-get install curl gnupg wget -y && curl -sL https://deb
 RUN cd ~ &&\
     export PHANTOM_JS="phantomjs-2.1.1-linux-x86_64" &&\
     wget https://github.com/Medium/phantomjs/releases/download/v2.1.1/$PHANTOM_JS.tar.bz2 &&\
-    sudo tar xvjf $PHANTOM_JS.tar.bz2 &&\
-    sudo mv $PHANTOM_JS /usr/local/share &&\
-    sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
+    tar xvjf $PHANTOM_JS.tar.bz2 &&\
+    mv $PHANTOM_JS /usr/local/share &&\
+    ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
 
 # Install nodemon
 RUN npm install -g nodemon
--- index.test.js
+++ index.test.js
@@ -1,16 +1,53 @@
 var expect = require('chai').expect;
+var assert = require('chai').assert;
 var should = require('chai').should();
 var bot = require('./index');
 const path = require('path');
 const ImageCreator = require('./createResult');
 var appRoot = path.resolve(__dirname);
+var io = require('socket.io-client');
+var connection;
+var client1;
 
- describe('bot', function(){
-    it ('should work!',function(){
+var socketURL = 'http://127.0.0.1:3000';
+
+var options = {
+    transports: ['websocket'],
+    'force new connection': true
+};
+
+
+describe('bot', function () {
+
+    it('should work!', function () {
         expect(true).to.be.true;
     });
-    it ('should have a database',function(){
-        bot.db.filename.should.equal(appRoot+'/whatkindofdeveloperareyou.db');
+    it('should have a database', function (done) {
+        bot.db.filename.should.equal(appRoot + '/whatkindofdeveloperareyou.db');
         bot.db.inMemoryOnly.should.equal(false);
+        bot.db.find({}, function (err, docs) {
+            assert.isNull(err);
+            docs.length.should.equal(0);
+            done();
+        });
     });
- });
+    
+    it('should be able to receive connections', function () {
+        client1 = io.connect(socketURL, options);
+        connection = client1.on('connect', function (data) {
+
+        });
+    });
+
+    it('should give me a welcome message', function (done) {
+        this.timeout(10000);
+        client1.on('message',function(message) {
+            message.text.should.equal(bot.welcomeMessage);
+            done();
+        });
+        client1.emit('message', {
+            id: "Test",
+            action: "init"
+        });
+    });
+});
